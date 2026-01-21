955bdfebdf8b808a68e00178e9dd302662bfeacc
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-21 14:29:05 -0300

    feat: configurar rotas da aplicação com react router

--- api-ecommerce-frontend/src/App.tsx
+++ api-ecommerce-frontend/src/App.tsx
@@ -1,11 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+
 import ProdutosPage from "./pages/ProdutosPage";
+import ProdutoDetalhePage from "./pages/ProdutoDetalhePage";
+import CarrinhoPage from "./pages/CarrinhoPage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
   return (
     <>
       <Header />
-      <ProdutosPage />
+      <Routes>
+        <Route path="/" element={<ProdutosPage />} />
+        <Route path="/produtos" element={<ProdutosPage />} />
+        <Route path="/produtos/:id" element={<ProdutoDetalhePage />} />
+        <Route path="/carrinho" element={<CarrinhoPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+      </Routes>
     </>
   );
 }
